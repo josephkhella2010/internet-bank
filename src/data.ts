211e7e7fe835c85dcd9d238928cbcdba2d1d8e18
joseph khella <josephkhella2010@gmail.com>
2026-09-22 19:27:47 +0300

    add trantions get a transtions and  get by id

---
+++ src/data.ts
@@ -0,0 +1,4 @@
+import transactions from "../data/transactions.json" with { type: "json" };
+import classifications from "../data/classifications.json" with { type: "json" };
+
+export { transactions, classifications };
